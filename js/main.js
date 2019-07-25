84a32e32cdc5d815ce8d236d1362677681e2d0b6
JayanthMogadampalli <jayu6766@gmail.com>
2019-07-25 09:57:32 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ function getDetails() {
             let output = '<h2 style="align:center;">Data</h2>';
             data.forEach(function (country) {
                 output += `
-    <div class="card">
-    <a href="countrydetails.html?countryname="+(${country.name})><img src="${country.flag}" alt="Avatar" style="width:100%"></a>
-    <div class="container">
-        <h4><b>${country.name}</b></h4>
-        <p>Architect & Engineer</p>
+        <div class="card">
+        <a href="countrydetails.html?countryname="+(${country.name})><img src="${country.flag}" class="img1" alt="Avatar" style="width:100%"></a>
+        <div class="container2">
+            <h4 id="heading3"><b>${country.name}</b></h4>
+            <p>Architect & Engineer</p>
+        </div>
     </div>
-</div>
-  `;
+      `;
             });
             document.getElementById('container1').innerHTML = output;
         })
